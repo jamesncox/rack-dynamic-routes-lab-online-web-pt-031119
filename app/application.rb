b307f0ab02bfa0e1eaabbe4b2e7db7d92de9cdef
James Cox <jamesncox@yahoo.com>
2019-06-04 17:27:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item = @@items.
+      if item = @@items.find {|
     else 
       resp.write "Route not found"
       resp.status = 404
